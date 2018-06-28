b9ef3c232b9caea706a015f62c1ffcfa4e415e71
RayCiel <loulan_9848@163.com>
2018-06-28 22:22:21 +0800

    fakeIR

--- build.bash
+++ build.bash
@@ -5,4 +5,4 @@ set -e
 cd "$(dirname "$0")"
 mkdir -p bin
 find ./src/com -regex ".*\(\.java\)$" | javac -d bin -classpath "lib/antlr-4.7.1-complete.jar" @/dev/stdin
-cp ./src/com/cLibrary.asm ./bin/MxCompiler/
+cp ./src/com/cLibrary.asm ./bin/com/
